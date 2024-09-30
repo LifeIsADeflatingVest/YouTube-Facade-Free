96fbc7e03f19c496a269953fbc0af8f0eaed355e
Chris <41416621+LifeIsADeflatingVest@users.noreply.github.com>
2024-09-30 16:44:18 +0300

    Add files via upload

--- youtube-facade-free.php
+++ youtube-facade-free.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: YouTube Video Facade: Free Version
  * Description: Replaces embedded YouTube videos with clickable facades using their thumbnails.
- * Version: 1.1
+ * Version: 1.2
  * Author: Chris Angelis, <a href="https://homeforfiction.com">Home for Fiction</a>.
  */
 
